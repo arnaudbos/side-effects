fe351d77a47d111cc9b980438a8ddac5dab1aaa8
Arnaud Bos <arnaud@arnaudbos.com>
2017-04-03 02:32:45 +0200

    Update and publish

--- js/main.js
+++ js/main.js
@@ -8,7 +8,21 @@ jQuery(document).ready(function(e) {
         e("html,body").animate({
             scrollTop: e(this.hash).offset().top
         }, 1e3)
-    })
-});
+    });
 
+    $('.scroll-to-top').hide();
+    //Check to see if the window is top if not then display button
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 200) {
+        $('.scroll-to-top').fadeIn();
+      } else {
+        $('.scroll-to-top').fadeOut();
+      }
+    });
 
+    //Click event to scroll to top
+    $('.scroll-to-top').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 800);
+      return false;
+    });
+  });
